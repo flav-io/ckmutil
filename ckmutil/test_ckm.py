304b36497eb77afaf0f25b4ea303d400676d485a
David Straub <david.straub@tum.de>
2017-11-08 15:17:53 +0100

    Add test for tree_to_wolfenstein

--- ckmutil/test_ckm.py
+++ ckmutil/test_ckm.py
@@ -27,6 +27,7 @@ class TestCKM(unittest.TestCase):
     v_s = ckm_standard(t12, t13, t23, delta)
     v_w = ckm_wolfenstein(laC, A, rhobar, etabar)
     v_t = ckm_tree(Vus, Vub, Vcb, gamma)
+    v_wt = ckm_wolfenstein(*tree_to_wolfenstein(Vus, Vub, Vcb, gamma))
     par_s = dict(t12=t12,t13=t13,t23=t23,delta=delta)
     par_w = dict(laC=laC,A=A,rhobar=rhobar,etabar=etabar)
     par_t = dict(Vus=Vus,Vub=Vub,Vcb=Vcb,gamma=gamma)
@@ -34,6 +35,7 @@ class TestCKM(unittest.TestCase):
     def test_ckm_parametrizations(self):
         np.testing.assert_almost_equal(self.v_t/self.v_s, np.ones((3,3)), decimal=5)
         np.testing.assert_almost_equal(self.v_t/self.v_w, np.ones((3,3)), decimal=5)
+        np.testing.assert_almost_equal(self.v_t/self.v_wt, np.ones((3,3)), decimal=5)
 
     def test_ckm_unitarity(self):
         np.testing.assert_almost_equal(np.dot(self.v_t,self.v_t.conj().T), np.eye(3), decimal=15)
