ea51fe1d1a24617d39702e818b841f2f83ef62ca
David Straub <straub@protonmail.com>
2023-08-06 21:08:39 +0200

    Modernize build system, version -> 0.4

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(name='ckmutil',
-      version='0.3.2',
-      author='David M. Straub',
-      author_email='david.straub@tum.de',
-      url='https://davidmstraub.github.io/ckmutil',
-      description='Useful functions for dealing with quark and lepton mixing matrices.',
-      long_description="""``ckmutil`` is a package containing useful functions
-      to deal with the Cabibbo-Kobayashi-Maskawa (CKM) quark mixing matrix or
-      the Pontecorvo-Maki-Nakagawa-Sakata (PMNS) lepton mixing matrix
-      in high energy physics.""",
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['numpy', 'setuptools>=3.3'],
-    )
+if __name__ == "__main__":
+  setup()
